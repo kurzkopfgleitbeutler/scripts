9f2a5ffa4ecd169fbd6f0c2ecf0367ac4824afe9
kurzkopfgleitbeutler <einfachzuverstehen@posteo.de>
2022-04-05 15:46:23 +0200

    fix checking for flag count

--- util/ato-flags.sh
+++ util/ato-flags.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
-if [ "$#" -lt "$num_args_min" ] || [ "$#" -gt "$num_args_max" ]
+if [ -n "$num_args_min" ] && [ -n "$num_args_max" ]
 then
-    printf "%b\n" "[ERROR] wrong number of arguments: should be between $num_args_min and $num_args_max\n$help"
-    exit 2
+    if [ "$#" -lt "$num_args_min" ] || [ "$#" -gt "$num_args_max" ]
+    then
+	printf "%b\n" "[ERROR] wrong number of arguments: should be between $num_args_min and $num_args_max\n$help"
+	exit 2
+    fi
 fi
